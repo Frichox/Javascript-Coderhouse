4795a8d31e8060065815f9f6a3f28afb8e4f24e6
Frichox <fedequinterod30@gmail.com>
2022-10-10 20:12:58 -0300

    Segunda pre-entrega

--- main.js
+++ main.js
@@ -2,21 +2,46 @@ let anio = Number (prompt("Ingrese el año de su vehículo"));
 
 let modelo;
 
-let tipoVehiculo = Number (prompt("Ingrese 1 si su vehículo es auto o 2 si es camioneta"));
+let tipoVehiculo = Number (prompt("Ingrese el número correspondiente según su vehículo: 1 Auto 2 Moto 3 Camioneta 4 Camión"));
 
 let base;
 
 
+function TipoDeVehiculo(tipo, baseimponible) {
+    this.tipo = tipo;
+    this.baseimponible = Number(baseimponible);
+}
+
+let vehiculoAuto = new TipoDeVehiculo("Auto", 1000)
+
+let vehiculoMoto = new TipoDeVehiculo("Moto", 800)
+
+let vehiculoCamioneta = new TipoDeVehiculo("Camioneta", 1200)
+
+let vehiculoCamion = new TipoDeVehiculo("Camion", 1500)
+
+let tiposdeVehiculo = [vehiculoAuto, vehiculoMoto, vehiculoCamioneta, vehiculoCamion]
+
+console.log("------ Tipos de vehículos ------")
+
+tiposdeVehiculo.forEach( function (item){
+    console.log (item) 
+})
 
 function costoModelo () {
 
-    if(anio<=2000) {
+    if(anio<2000 && anio>=1990) {
             modelo = 2000
+            console.log ("Prima por modelo " + "$" + modelo )       
+    }else if (anio>=2000 && anio<=2010){ 
+            modelo = 2500
             console.log ("Prima por modelo " + "$" + modelo )
-            
-    }else if (anio>=2000){ 
-            modelo = 3000
-            console.log ("Prima por modelo " + "$" + modelo )
+    }else if (anio>2010 && anio<=2020){ 
+        modelo = 3000
+        console.log ("Prima por modelo " + "$" + modelo )
+    }else if (anio>2020){ 
+        modelo = 3500
+        console.log ("Prima por modelo " + "$" + modelo )
     }else { 
         alert ("El año ingresado no es válido")
 
@@ -29,10 +54,16 @@ costoModelo ();
 function costoTipoVehiculo () {
    
     if(tipoVehiculo==1) {
-        base = 2500
-        console.log ("Prima por tipo de vehículo " + "$" + base )
+        base = Number (vehiculoAuto.baseimponible)
+        console.log ("Prima por tipo de vehículo " + "$" + base ) 
     }else if (tipoVehiculo==2){ 
-        base = 3500
+        base = Number (vehiculoMoto.baseimponible)
+        console.log ("Prima por tipo de vehículo " + "$" + base )
+    }else if (tipoVehiculo==3){ 
+        base = Number (vehiculoCamioneta.baseimponible)
+        console.log ("Prima por tipo de vehículo " + "$" + base )
+    }else if (tipoVehiculo==4){ 
+        base = Number (vehiculoCamion.baseimponible)
         console.log ("Prima por tipo de vehículo " + "$" + base )
     }else{ 
         alert ("Seleccione la opción correcta")
@@ -50,7 +81,9 @@ let seguro = prima + impuestos;
 
 let cuotas = 6
 
-let cuotaSeguro = seguro / cuotas;
+let cuotaSeguro = seguro / cuotas;1
+
+
 
 console.log ( "Prima total " + "$" + prima)
 console.log ( "Impuestos " + "$" + impuestos)
@@ -58,7 +91,7 @@ console.log ( "Costo total del seguro " + "$" + seguro)
 console.log ( "Costo por cuota " + "$" + cuotaSeguro) 
  
 
- while(seguro>=6000){
+ while(seguro>=4500){
     console.log ("Si desea abonar el total del seguro de contado efectivo tendra 10% de descuento");
     break; 
  }
